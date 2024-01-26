1701b536634f38a4ee52a0003414914eb37afef7
Alessandro Montanari <alessandro.monta@gmail.com>
2024-01-26 12:54:03 +0100

    edit publishbulk.sh

--- publishbulk.sh
+++ publishbulk.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 set -e
-data=$(ls -U telemetries/ | head -9)
+data=$(find -type f -wholename "$1" | head -9)
 while [ "$data" ]
 do
-	`cd telemetries; python3 ../helper.py $data >  /tmp/mess.json`
+	python3 ./helper.py $data >  /tmp/mess.json
 	/usr/local/bin/aws sns publish-batch --profile=rurall --publish-batch-request-entries file:///tmp/mess.json --topic-arn="arn:aws:sns:eu-west-1:902738125373:sdf-ingestion" > /dev/null
 	echo $data
-	cd telemetries/
 	rm $data
-	cd ..
-	data=$(ls -U telemetries/ | head -9)
+	data=$(find -type f -wholename "$1" | head -9)
 done
